ef3a70bf0a6915d0da6d25a97e3222732afc532b
Adriano Oliveira <drprado2@gmail.com>
2020-10-04 18:49:33 -0300

    Versão com projeto web criado com create-react-app e com eslint e prettier configurados

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -3,29 +3,46 @@ module.exports = {
     browser: true,
     es2020: true,
     node: true,
-    jest: true
+    jest: true,
+    "es6": true
   },
   extends: [
-    'plugin:react/recommended',
-    'standard',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/standard',
-    'prettier/react'
+    "plugin:react/recommended",
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
   ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+    "__DEV__": "readonly"
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: 11,
+    "ecmaVersion": 2018,
     sourceType: 'module'
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier', "better-styled-components", "react-hooks"],
   rules: {
-    '@typescript-eslint/camelcase': 'off',
-    'prettier/prettier': 'error',
-    'space-before-function-paren': 'off'
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-filename-extension": [1, { "extensions": [".tsx"] }],
+    "import/prefer-default-export": "off",
+    "no-use-before-define": "off",
+    "prettier/prettier": "error",
+    "better-styled-components/sort-declarations-alphabetically": 2,
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "ts": "never",
+        "tsx": "never"
+      }
+    ]
   },
   settings: {
     'import/resolver': {
--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,16 +1,7 @@
-import React, { useEffect } from 'react'
-import api from '@gobarber/axios-config'
+import React from 'react'
 
-// import { Container } from './styles';
-
-const App: React.FC = () => {
-  useEffect(() => {
-    api.get('/').then(response => {
-      console.log(response)
-    })
-  }, [])
-
-  return <h1>Hello World</h1>
+function App() {
+  return <h1>Olá</h1>
 }
 
 export default App
